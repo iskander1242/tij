5c80535c4716385ec7e3fefa9e1610c365d9a7ca
Alexander Sokolov <alexander.sokolov@rednavis.com>
2018-08-15 14:34:46 +0300

    using Callable Interface

---
+++ src/main/java/concurency_14_08/CallableDemo.java
@@ -0,0 +1,45 @@
+package concurency_14_08;
+
+import java.util.ArrayList;
+import java.util.concurrent.*;
+
+public class CallableDemo {
+
+    static final int NUMBER_OF_THREADS = 15;
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        ArrayList<Future<String>> futureList = new ArrayList<>();
+        for (int i=0;i< NUMBER_OF_THREADS; i++){
+            futureList.add(executorService.submit(new TaskWithRecult(i)));
+        }
+        for (Future<String>  i : futureList) {
+            try {
+                System.out.println(i.get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return;
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } finally {
+                executorService.shutdown();
+            }
+
+        }
+    }
+
+}
+
+class TaskWithRecult implements Callable<String> {
+    private final int id;
+
+    public TaskWithRecult(int id) {
+        this.id = id;
+
+    }
+
+    @Override
+    public String call() throws Exception {
+        return "return TaskWithResult id=" + id;
+    }
+}
